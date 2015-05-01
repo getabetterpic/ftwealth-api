8d741a58a92681d018c900e4ebac822c3e0c7429
getabetterpic <dan@danandjensmith.com>
2015-05-01 09:10:52 -0400

    Got basic FI endpoint working

--- app/controllers/api/v1/financial_institutions_controller.rb
+++ app/controllers/api/v1/financial_institutions_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::FinancialInstitutionsController < Api::V1::ApplicationController
+class Api::V1::FinancialInstitutionsController < ApplicationController
   before_action :set_financial_institution, only: [:show, :update, :destroy]
 
   # GET /financial_institutions
